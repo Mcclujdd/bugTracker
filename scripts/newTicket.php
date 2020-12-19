1132785e5e48818a75a952510f4bc759c83f69f9
Jason McClurg <mcclujdd@gmail.com>
2020-12-19 09:27:51 -0500

    fixed not-posting issue

--- scripts/newTicket.php
+++ scripts/newTicket.php
@@ -1,13 +1,14 @@
-<?php?> <section class="container grey-text">
+<?php?>
+<section class="container grey-text">
 
-  <form action="index.php" method="POST">
+  <form action="index.php" action="index.php" method="POST">
     <label>Error Message</label>
     <input type="text" name="error">
     <label>Description of Error</label>
     <textarea type="text" name="description"></textarea>
     <!-- <label>Placeholder</label>
     <input type="text" name="placeholder"> -->
-  </form>
   <div>
     <input type="submit" name="submit" value="Submit New Ticket" class="btn btn-primary bg-success">
   </div>
+  </form>
