0f74b56d732d2a92e80a08b758bafa02e94b7f27
Jason McClurg <mcclujdd@gmail.com>
2021-01-01 18:29:58 -0500

    modularization of navbar

--- index.php
+++ index.php
@@ -19,7 +19,7 @@ $description=mysqli_real_escape_string($conn, $_POST['description']);
 //query for inserting data
 $sqlWrite="INSERT INTO tickets(error,description) VALUES('$error,$description')";
 
-//submit to database and test for errors
+//submit to database and display errors
 if (mysqli_query($conn, $sqlWrite)){
   //success
   header('Location: index.php');
@@ -63,27 +63,7 @@ mysqli_close($conn);
           <h1>Dashboard</h1>
         </div>
 
-<!-- navbar -->
-
-        <nav class="navbar navbar-toggleable-md navbar-light">
-          <form class="form-inline my-2 my-lg-0">
-            <input class="form-control mr-sm-2" type="text" placeholder="Search">
-            <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search Tickets</button>
-          </form>
-          <button class="navbar-toggler ml-auto" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-          </button>
-
-          <div class="collapse navbar-collapse" id="navbarSupportedContent">
-            <div class="navbar-nav">
-                <a class="nav-item nav-link active" href="#">Home</a>
-                <a class="nav-item nav-link" href="#">Tickets</a>
-                <a class="nav-item nav-link" href="#">Settings</a>
-            </div>
-
-
-          </div>
-        </nav>
+<<?php require_once(__ROOT__.'/navbar.php'); ?>
 
 <!-- Main Content -->
         <div class="container-fluid " style="background: rgba(60, 136, 63, 0.2)">
---
+++ navbar.php
@@ -0,0 +1,24 @@
+<?php
+
+ ?>
+ <!-- navbar -->
+
+        <nav class="navbar navbar-toggleable-md navbar-light">
+          <form class="form-inline my-2 my-lg-0">
+            <input class="form-control mr-sm-2" type="text" placeholder="Search">
+            <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search Tickets</button>
+          </form>
+          <button class="navbar-toggler ml-auto" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+          </button>
+
+          <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <div class="navbar-nav">
+                <a class="nav-item nav-link active" href="#">Home</a>
+                <a class="nav-item nav-link" href="#">Tickets</a>
+                <a class="nav-item nav-link" href="#">Settings</a>
+            </div>
+
+
+          </div>
+        </nav>
